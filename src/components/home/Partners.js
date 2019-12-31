1077fbd901e9c5823fd88562e02bdb30c0fdd31b
Evan Dickinson <evan@finhaven.com>
2019-12-30 16:33:12 -0800

    Remove Interstellar logo from our partners

--- src/components/home/Partners.js
+++ src/components/home/Partners.js
@@ -14,18 +14,18 @@ const Partners = () => {
             { maxWidth: width + "px" }
         );
     };
-    
+
     return (
         <section id="partners" className="partners-section text-center">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-12 mx-auto">
                     <h2 className="mb-4">PARTNERS</h2>
-                        
+
                     <div className="row d-lg-none px-3">
                         <div className="col-12"><img style={maxWidth(155)} src={partner1} alt="fihaven-technology-partner1" /></div>
                         <div className="col-12"><img style={maxWidth(125)} src={partner2} alt="fihaven-technology-partner2" /></div>
-                        <div className="col-12"><img style={maxWidth(98)} src={partner3} alt="fihaven-technology-partner3" /></div>
+                        {/*<div className="col-12"><img style={maxWidth(98)} src={partner3} alt="fihaven-technology-partner3" /></div>*/}
                         <div className="col-12"><img style={maxWidth(131)} src={partner4} alt="fihaven-technology-partner4" /></div>
                         <div className="col-12"><img style={maxWidth(54)} src={partner5} alt="fihaven-technology-partner5" /></div>
                         <div className="col-12"><img style={maxWidth(121)} src={partner6} alt="fihaven-technology-partner6" /></div>
@@ -36,7 +36,7 @@ const Partners = () => {
                         <div className="d-flex justify-content-around align-items-center flex-wrap px-5">
                         <img style={maxWidth(155)} src={partner1} alt="fihaven-technology-partner1" />
                         <img style={maxWidth(125)} src={partner2} alt="fihaven-technology-partner2" />
-                        <img style={maxWidth(98)} src={partner3} alt="fihaven-technology-partner3" />
+                        {/*<img style={maxWidth(98)} src={partner3} alt="fihaven-technology-partner3" />*/}
                         <img style={maxWidth(131)} src={partner4} alt="fihaven-technology-partner4" />
                         <img style={maxWidth(54)} src={partner5} alt="fihaven-technology-partner5" />
                         <img style={maxWidth(121)} src={partner6} alt="fihaven-technology-partner6" />
