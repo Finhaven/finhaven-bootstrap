b5a4ecf7cbcd11f69f875ea56069cec63a747ca2
Evan Dickinson <evan@finhaven.com>
2019-12-30 16:37:46 -0800

    Add names to the partner images

--- src/components/home/Partners.js
+++ src/components/home/Partners.js
@@ -23,24 +23,24 @@ const Partners = () => {
                     <h2 className="mb-4">PARTNERS</h2>
 
                     <div className="row d-lg-none px-3">
-                        <div className="col-12"><img style={maxWidth(155)} src={partner1} alt="fihaven-technology-partner1" /></div>
-                        <div className="col-12"><img style={maxWidth(125)} src={partner2} alt="fihaven-technology-partner2" /></div>
-                        {/*<div className="col-12"><img style={maxWidth(98)} src={partner3} alt="fihaven-technology-partner3" /></div>*/}
-                        <div className="col-12"><img style={maxWidth(131)} src={partner4} alt="fihaven-technology-partner4" /></div>
-                        <div className="col-12"><img style={maxWidth(54)} src={partner5} alt="fihaven-technology-partner5" /></div>
-                        <div className="col-12"><img style={maxWidth(121)} src={partner6} alt="fihaven-technology-partner6" /></div>
-                        <div className="col-12"><img style={maxWidth(86)} src={partner7} alt="fihaven-technology-partner7" /></div>
+                        <div className="col-12"><img style={maxWidth(155)} src={partner1} alt="Venture for Canada" /></div>
+                        <div className="col-12"><img style={maxWidth(125)} src={partner2} alt="The White Company" /></div>
+                        {/*<div className="col-12"><img style={maxWidth(98)} src={partner3} alt="Intersteallar" /></div>*/}
+                        <div className="col-12"><img style={maxWidth(131)} src={partner4} alt="Medici Investment" /></div>
+                        <div className="col-12"><img style={maxWidth(54)} src={partner5} alt="7Gate Academy" /></div>
+                        <div className="col-12"><img style={maxWidth(121)} src={partner6} alt="Dunamu &amp; Partners" /></div>
+                        <div className="col-12"><img style={maxWidth(86)} src={partner7} alt="Lanebury Growth Capital" /></div>
                     </div>
 
                     <div className="row d-lg-block d-none">
                         <div className="d-flex justify-content-around align-items-center flex-wrap px-5">
-                        <img style={maxWidth(155)} src={partner1} alt="fihaven-technology-partner1" />
-                        <img style={maxWidth(125)} src={partner2} alt="fihaven-technology-partner2" />
-                        {/*<img style={maxWidth(98)} src={partner3} alt="fihaven-technology-partner3" />*/}
-                        <img style={maxWidth(131)} src={partner4} alt="fihaven-technology-partner4" />
-                        <img style={maxWidth(54)} src={partner5} alt="fihaven-technology-partner5" />
-                        <img style={maxWidth(121)} src={partner6} alt="fihaven-technology-partner6" />
-                        <img style={maxWidth(86)} src={partner7} alt="fihaven-technology-partner7" />
+                        <img style={maxWidth(155)} src={partner1} alt="Venture for Canada" />
+                        <img style={maxWidth(125)} src={partner2} alt="The White Company" />
+                        {/*<img style={maxWidth(98)} src={partner3} alt="Intersteallar" />*/}
+                        <img style={maxWidth(131)} src={partner4} alt="Medici Investment" />
+                        <img style={maxWidth(54)} src={partner5} alt="7Gate Academy" />
+                        <img style={maxWidth(121)} src={partner6} alt="Dunamu &amp; Partners" />
+                        <img style={maxWidth(86)} src={partner7} alt="Lanebury Growth Capital" />
                     </div>
                     </div>
                     </div>
